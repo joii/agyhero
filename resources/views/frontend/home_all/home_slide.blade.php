f5eb3a7e98b15086c202c65ff06b606ba48e7e23
phatchara yubon <contact.puipui@gmail.com>
2024-04-05 20:28:14 +0700

    Update banner  text and css

--- resources/views/frontend/home_all/home_slide.blade.php
+++ resources/views/frontend/home_all/home_slide.blade.php
@@ -4,13 +4,13 @@
         <div class="single-slider slider-bg d-flex  slide1" >
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-12">
                         <div class="slider-content ">
                             <img src="{{ asset('frontend/assets/img/logo/logo_light_bg.png')}}" alt="" title=""  width="190" class="mb-70 d-none d-sm-none d-md-block">
                             <h4 data-animation="fadeInUp" data-delay=".3s" class="slide-header d-none d-sm-none d-md-block">Professional Team <br/><span class="txt-purple">20 Years Experience</span><br/></h4>
-                            <h4 data-animation="fadeInUp" data-delay=".4s" class="slide-header d-none d-sm-none d-md-block">Innovation-Oriented <br/><span class="txt-purple">Age Care</span><br/></h4> 
+                            <h4 data-animation="fadeInUp" data-delay=".4s" class="slide-header d-none d-sm-none d-md-block">Innovation-Oriented<br/><span class="txt-purple">Age Care</span><br/></h4>
                             <h4 data-animation="fadeInUp" data-delay=".5s" class="slide-heade d-none d-sm-none d-md-block">Human Care at <span class="slide-txt-purple">Heart</span></h4>
-                           
+
                         </div>
                     </div>
                 </div>
@@ -19,11 +19,11 @@
         <div class="single-slider slider-bg d-flex slide2">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-12">
                         <div class="slider-content ">
                             <img src="{{ asset('frontend/assets/img/logo/logo_light_bg.png')}}" alt="" title="" data-animation="fadeInUp" data-delay=".2s" width="190" class="mb-70 d-none d-sm-none d-md-block">
                             <h3 data-animation="fadeInUp" data-delay=".3s" class="slide-header d-none d-sm-none d-md-block">Professional <br/>Team <span class="txt-purple">20 Years <br/>Experience</span></h3>
-                            
+
                         </div>
                     </div>
                 </div>
@@ -32,11 +32,11 @@
         <div class="single-slider slider-bg d-flex slide3" >
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-12">
                         <div class="slider-content">
                             <img src="{{ asset('frontend/assets/img/logo.png')}}" alt="" title="" data-animation="fadeInUp" data-delay=".2s" width="190" class="mb-70 d-none d-sm-none d-md-block">
-                            <h3 data-animation="fadeInUp" data-delay=".3s" class="text-white slide-header d-none d-sm-none d-md-block">Innovation <br/>Ampering <br/>Age Care</h3> 
-        
+                            <h3 data-animation="fadeInUp" data-delay=".3s" class="text-white slide-header d-none d-sm-none d-md-block">Innovation-Oriented <br/>Age Care</h3>
+
                         </div>
                     </div>
                 </div>
@@ -45,11 +45,11 @@
         <div class="single-slider slider-bg d-flex slide4">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-12">
                         <div class="slider-content">
                             <img src="{{ asset('frontend/assets/img/logo/logo_light_bg.png')}}" alt="" title="" data-animation="fadeInUp" data-delay=".2s" width="190" class="mb-70 d-none d-sm-none d-md-block">
                             <h3 data-animation="fadeInUp" data-delay=".3s" class="slide-header d-none d-sm-none d-md-block">Human Care <br/>at <span class="slide-txt-purple">Heart</span></h3>
-        
+
                         </div>
                     </div>
                 </div>
